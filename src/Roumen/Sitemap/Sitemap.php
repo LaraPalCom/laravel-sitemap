75d978f10a31a2d9a0a1a2e5ed6836806904f39a
Vladimir Jelovac <vladimir.jelovac@jeftinodosajta.com>
2014-01-18 02:00:12 +0100

    Added cache support.

--- src/Roumen/Sitemap/Sitemap.php
+++ src/Roumen/Sitemap/Sitemap.php
@@ -1,4 +1,7 @@
-<?php namespace Roumen\Sitemap;
+<?php
+
+namespace Roumen\Sitemap;
+
 /**
  * Sitemap class for laravel-sitemap package.
  *
@@ -7,19 +10,47 @@
  * @link http://roumen.it/projects/laravel-sitemap
  * @license http://opensource.org/licenses/mit-license.php MIT License
  */
-
-use Config;
-use Response;
-use View;
-use File;
+use Config,
+    Response,
+    View,
+    File,
+    Cache;
 
 class Sitemap
 {
 
-    protected $items = array();
-    protected $title;
-    protected $link;
+    /**
+     * Model instance
+     * @var Model $model 
+     */
+    protected $model = null;
 
+    /**
+     * Using constructor we populate our model from configuration file
+     * @param array $config
+     */
+    public function __construct(array $config)
+    {
+        $this->model = new Model($config);
+    }
+
+    /**
+     * Set cache options
+     * 
+     * @param string $key
+     * @param integer $duration
+     * @param boolean $useCache
+     */
+    public function setCache($key = null, $duration = null, $useCache = true)
+    {
+        $this->model->setUseCache($useCache);
+        if ($key !== null) {
+            $this->model->setCacheKey($key);
+        }
+        if ($duration !== null) {
+            $this->model->setCacheDuration($duration);
+        }
+    }
 
     /**
      * Add new sitemap item to $items array
@@ -35,16 +66,17 @@ class Sitemap
      */
     public function add($loc, $lastmod = null, $priority = null, $freq = null, $image = array(), $title = null)
     {
-        $this->items[] = array(
-            'loc' => $loc,
-            'lastmod' => $lastmod,
-            'priority' => $priority,
-            'freq' => $freq,
-            'image' => $image,
-            'title'=> $title
+        $this->model->setItems(
+                array(
+                    'loc' => $loc,
+                    'lastmod' => $lastmod,
+                    'priority' => $priority,
+                    'freq' => $freq,
+                    'image' => $image,
+                    'title' => $title
+                )
         );
     }
-
 
     /**
      * Returns document with all sitemap items from $items array
@@ -68,30 +100,38 @@ class Sitemap
      */
     public function generate($format = 'xml')
     {
-        if (empty($this->link)) $this->link = Config::get('app.url');
-        if (empty($this->title)) $this->title = 'Sitemap for ' . $this->link;
+        if (empty($this->model->getLink())) {
+            $this->model->setLink(Config::get('app.url'));
+        }
+
+        if (empty($this->model->getTitle())) {
+            $this->model->setTitle(('Sitemap for ' . $this->model->getLink()));
+        }
 
         $channel = array(
-            'title' => $this->title,
-            'link' => $this->link
+            'title' => $this->model->getTitle(),
+            'link' => $this->model->getLink()
         );
 
-        switch ($format)
-        {
+        if ($this->model->getUseCache()) {
+            if (Cache::has($this->model->getCacheKey())) {
+                $this->model->setItems(Cache::get($this->model->getCacheKey()));
+            } else {
+                Cache::put($this->model->getCacheKey(), $this->model->getItems(), $this->model->getCacheDuration());
+            }
+        }
+
+        switch ($format) {
             case 'ror-rss':
-                return array('content' => View::make('sitemap::ror-rss', array('items' => $this->items, 'channel' => $channel)), 'headers' => array('Content-type' => 'text/rss+xml; charset=utf-8') );
-                break;
+                return array('content' => View::make('sitemap::ror-rss', array('items' => $this->model->getItems(), 'channel' => $channel)), 'headers' => array('Content-type' => 'text/rss+xml; charset=utf-8'));
             case 'ror-rdf':
-                return array('content' => View::make('sitemap::ror-rdf', array('items' => $this->items, 'channel' => $channel)), 'headers' => array('Content-type' => 'text/rdf+xml; charset=utf-8') );
-                break;
+                return array('content' => View::make('sitemap::ror-rdf', array('items' => $this->model->getItems(), 'channel' => $channel)), 'headers' => array('Content-type' => 'text/rdf+xml; charset=utf-8'));
             case 'html':
-                return array('content' => View::make('sitemap::html', array('items' => $this->items, 'channel' => $channel)), 'headers' => array('Content-type' => 'text/html') );
-                break;
+                return array('content' => View::make('sitemap::html', array('items' => $this->model->getItems(), 'channel' => $channel)), 'headers' => array('Content-type' => 'text/html'));
             case 'txt':
-                return array('content' => View::make('sitemap::txt', array('items' => $this->items, 'channel' => $channel)), 'headers' => array('Content-type' => 'text/plain') );
-                break;
+                return array('content' => View::make('sitemap::txt', array('items' => $this->model->getItems(), 'channel' => $channel)), 'headers' => array('Content-type' => 'text/plain'));
             default:
-                return array('content' => View::make('sitemap::xml', array('items' => $this->items, 'channel' => $channel)), 'headers' => array('Content-type' => 'text/xml; charset=utf-8') );
+                return array('content' => View::make('sitemap::xml', array('items' => $this->model->getItems(), 'channel' => $channel)), 'headers' => array('Content-type' => 'text/xml; charset=utf-8'));
         }
     }
 
@@ -107,16 +147,13 @@ class Sitemap
     {
         $data = $this->generate($format);
 
-        if ($format == 'ror-rss' || $format == 'ror-rdf')
-        {
+        if ($format == 'ror-rss' || $format == 'ror-rdf') {
             $format = 'xml';
         }
 
-        $file = public_path() . DIRECTORY_SEPARATOR . $filename . '.' .$format;
+        $file = public_path() . DIRECTORY_SEPARATOR . $filename . '.' . $format;
 
         File::put($file, $data['content']);
-
-        $this->items = array();
     }
 
 }
