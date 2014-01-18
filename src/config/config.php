89507d37bc09de80fa165536dd84faed65001f4e
Vladimir Jelovac <vladimir.jelovac@jeftinodosajta.com>
2014-01-18 02:34:15 +0100

    Re added config dir.

---
+++ src/config/config.php
@@ -0,0 +1,8 @@
+<?php
+
+/* Simple configuration file for Laravel Sitemap package */
+return array(
+    'use_cache' => false,
+    'cache_key' => 'Laravel.Sitemap.',
+    'cache_duration' => 3600,
+);
