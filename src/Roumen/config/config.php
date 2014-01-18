54bda8e921b605c22dee26485dc7a1eeeec33e07
Vladimir Jelovac <vladimir.jelovac@jeftinodosajta.com>
2014-01-18 02:14:31 +0100

    Moved config directory.

--- src/Roumen/config/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-/* Simple configuration file for Laravel Sitemap package */
-return array(
-    'use_cache' => false,
-    'cache_key' => 'Laravel.Sitemap.',
-    'cache_duration' => 3600,
-);
