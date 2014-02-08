1ad376755777b4776fd53c59bc4612238272ffcd
Roumen Damianoff <roumen@dawebs.com>
2014-02-08 23:30:23 +0200

    fixed sitemapindex.blade.php

--- src/views/sitemapindex.blade.php
+++ src/views/sitemapindex.blade.php
@@ -1,9 +1,9 @@
 {{ '<?xml version="1.0" encoding="UTF-8"?>'."\n" }}
 <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-@foreach($items as $item)
+@foreach($sitemaps as $sitemap)
 	<sitemap>
-		<loc>{{ $item['loc'] }}</loc>
-		<lastmod>{{ $item['loc'] }}</lastmod>
+		<loc>{{ $sitemap['loc'] }}</loc>
+		@if($sitemap['lastmod'] !== null)<lastmod>{{ date('Y-m-d\TH:i:sP', strtotime($sitemap['lastmod'])) }}</lastmod>@endif
 	</sitemap>
 @endforeach
 </sitemapindex>
