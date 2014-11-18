f8afbb1e9416137bdfe81d49aa16cd64e840778a
Anton Chernyavski <i.splinter@i.ua>
2014-11-18 16:43:19 +0200

    fix sitemap caching for different domains

--- src/config/config.php
+++ src/config/config.php
@@ -3,6 +3,6 @@
 /* Simple configuration file for Laravel Sitemap package */
 return array(
     'use_cache' => false,
-    'cache_key' => 'Laravel.Sitemap.',
+    'cache_key' => 'Laravel.Sitemap.'.\Request::getHttpHost(),
     'cache_duration' => 3600,
 );
