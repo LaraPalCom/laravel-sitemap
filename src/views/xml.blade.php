fed0bda681c6744a0c8bf8c22c3bd421e8728f0f
Kia King Ishii <kia.king.08@gmail.com>
2015-02-25 15:47:32 +0900

    Fix translations not working correctly.
    Add xmlns:xhtml to avoid error and fix simple typo.

--- src/views/xml.blade.php
+++ src/views/xml.blade.php
@@ -1,12 +1,12 @@
 {!! '<'.'?'.'xml version="1.0" encoding="UTF-8"?>' !!}
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"{{ isset($item['translations']) ? ' xmlns:xhtml="http://www.w3.org/1999/xhtml"' }}>
 @foreach($items as $item)
   <url>
     <loc>{{ $item['loc'] }}</loc>
 <?php
 
-if (!empty($item['translation'])) {
-  foreach ($item['translation'] as $translation) {
+if (!empty($item['translations'])) {
+  foreach ($item['translations'] as $translation) {
     echo "\t\t" . '<xhtml:link rel="alternate" hreflang="' . $translation['language'] . '" href="' . $translation['url'] . '" />' . "\n";
   }
 }
