d2f72a0c4df21a100ae9200d9267cd482c53c985
Roumen Damianoff <roumen@dawebs.com>
2015-02-25 12:44:36 +0200

    fixed view

--- src/views/xml.blade.php
+++ src/views/xml.blade.php
@@ -1,5 +1,5 @@
 {!! '<'.'?'.'xml version="1.0" encoding="UTF-8"?>' !!}
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"{{ isset($item['translations']) ? ' xmlns:xhtml="http://www.w3.org/1999/xhtml"' }}>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"{{ isset($item['translations']) ? ' xmlns:xhtml="http://www.w3.org/1999/xhtml"' : '' }}>
 @foreach($items as $item)
   <url>
     <loc>{{ $item['loc'] }}</loc>
