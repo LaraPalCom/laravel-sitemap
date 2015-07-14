3f422656bdc980edf836f0426ebf800957e73f07
Sam Millar <sam@millar.io>
2015-07-14 10:04:57 +0100

    Add the video namespace to XML view
    Erroneously overlooked this in PR #73 – apologies!

--- src/views/xml.blade.php
+++ src/views/xml.blade.php
@@ -2,6 +2,7 @@
 <urlset
   xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
   xmlns:image="http://www.google.com/schemas/sitemap-image/1.1"
+  xmlns:video="http://www.google.com/schemas/sitemap-video/1.1"
   xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
   xmlns:xhtml="http://www.w3.org/1999/xhtml"
   xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
