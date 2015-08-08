c42ba3a9a693b25ce2276557e3f310e8068a931d
Rob Attfield <robert.attfield@eyemagnet.com>
2015-08-08 23:56:57 +1200

    Added extra declarations for xml sitemap render file.

--- src/views/xml.php
+++ src/views/xml.php
@@ -1,5 +1,5 @@
-<?= '<'.'?'.'xml-stylesheet type="text/xsl" href="/xsl/main-sitemap.xsl"?>'."\n" ?>
-<?= '<'.'?'.'xml version="1.0" encoding="UTF-8"?>'."\n" ?>
+<?= '<'.'?'.'xml version="1.0" encoding="UTF-8"?>' ?><?= '<'.'?'.'xml-stylesheet type="text/xsl" href="/xsl/main-sitemap.xslt"?>'."\n" ?>
+
 <urlset
     xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
     xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
