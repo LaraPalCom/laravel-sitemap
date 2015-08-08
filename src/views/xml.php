995b46b399271ca036d2eb497ca931b620d2d9c3
Rob Attfield <robert.attfield@eyemagnet.com>
2015-08-08 21:53:37 +1200

    Altered urlset xml element.

--- src/views/xml.php
+++ src/views/xml.php
@@ -1,12 +1,11 @@
 <?= '<'.'?'.'version="1.0" encoding="UTF-8"?>'."\n" ?>
 <?= '<'.'?'.'xml-stylesheet type="text/xsl" href="../xsl/main-sitemap.xsl"?>'."\n" ?>
-<urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
 <?php foreach($items as $item) : ?>
   <url>
     <loc>
       <?= $item['loc'] ?>
     </loc>
-  </url>
 <?php
 
 if (!empty($item['translations'])) {
