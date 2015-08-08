cac9187991013132319f35d96ac918756d34e511
Rob Attfield <robert.attfield@eyemagnet.com>
2015-08-09 00:08:50 +1200

    Fixed .xsl extension.

--- src/views/xml.php
+++ src/views/xml.php
@@ -1,4 +1,4 @@
-<?= '<'.'?'.'xml version="1.0" encoding="UTF-8"?>' ?><?= '<'.'?'.'xml-stylesheet type="text/xsl" href="/xsl/main-sitemap.xslt"?>'."\n" ?>
+<?= '<'.'?'.'xml version="1.0" encoding="UTF-8"?>' ?><?= '<'.'?'.'xml-stylesheet type="text/xsl" href="/xsl/main-sitemap.xsl"?>'."\n" ?>
 
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
   <?php foreach($items as $item) : ?>
