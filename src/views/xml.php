fb8109c21f2bf6fdce7f7f7104da020308c103e0
Rob Attfield <robert.attfield@eyemagnet.com>
2015-08-08 22:54:50 +1200

    Moved stylesheet declaration to top of xml render file.

--- src/views/xml.php
+++ src/views/xml.php
@@ -1,5 +1,5 @@
-<?= '<'.'?'.'xml version="1.0" encoding="UTF-8"?>'."\n" ?>
 <?= '<'.'?'.'xml-stylesheet type="text/xsl" href="../xsl/main-sitemap.xsl"?>'."\n" ?>
+<?= '<'.'?'.'xml version="1.0" encoding="UTF-8"?>'."\n" ?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
   <?php foreach($items as $item) : ?>
     <url>
