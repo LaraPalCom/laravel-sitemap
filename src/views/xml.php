00ef0dfb378f086e52e027ed64a909a69a3e3217
Rob Attfield <robert.attfield@eyemagnet.com>
2015-08-09 00:03:12 +1200

    Altered urlset declarations.

--- src/views/xml.php
+++ src/views/xml.php
@@ -1,12 +1,6 @@
 <?= '<'.'?'.'xml version="1.0" encoding="UTF-8"?>' ?><?= '<'.'?'.'xml-stylesheet type="text/xsl" href="/xsl/main-sitemap.xslt"?>'."\n" ?>
 
-<urlset
-    xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-    xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"
-    xmlns:xhtml="http://www.w3.org/1999/xhtml"
-    xmlns:image="http://www.google.com/schemas/sitemap-image/1.1"
-    xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
   <?php foreach($items as $item) : ?>
     <url>
       <loc><?= $item['loc'] ?></loc>
