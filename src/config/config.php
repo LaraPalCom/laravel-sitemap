8c68e119af0299dab44b4a6d993ebb67c6c8eee4
Roumen Damianoff <roumen@dawebs.com>
2015-12-17 19:14:54 +0200

    updated config.php
    - added new option use_limit_size

--- src/config/config.php
+++ src/config/config.php
@@ -3,7 +3,8 @@
 /* Simple configuration file for Laravel Sitemap package */
 return [
     'use_cache'      => false,
-    'cache_key'      => 'Laravel.Sitemap.' . config('app.url'),
+    'cache_key'      => 'laravel-sitemap.' . config('app.url'),
     'cache_duration' => 3600,
     'escaping'       => true,
+    'use_limit_size'   => false,
 ];
