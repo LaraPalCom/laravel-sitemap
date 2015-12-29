e42b7f09c6eb76bb74bd2f0b597c0c46062a6970
Roumen Damianoff <roumen@dawebs.com>
2015-12-29 17:16:02 +0200

    added custom xsl styles
    added option to use custom xsl styles

--- src/config/config.php
+++ src/config/config.php
@@ -2,9 +2,10 @@
 
 /* Simple configuration file for Laravel Sitemap package */
 return [
-    'use_cache'      => false,
-    'cache_key'      => 'laravel-sitemap.' . config('app.url'),
-    'cache_duration' => 3600,
-    'escaping'       => true,
-    'use_limit_size'   => false,
+	'use_cache'			=> 	false,
+	'cache_key'			=> 	'laravel-sitemap.' . config('app.url'),
+	'cache_duration'	=> 	3600,
+	'escaping'			=> 	true,
+	'use_limit_size'	=> 	false,
+	'use_styles'		=> 	true,
 ];
