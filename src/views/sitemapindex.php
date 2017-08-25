142f9d8d2370a82c589894d230b55b3703d69ad2
Rumen Damyanov <r@alfamatter.com>
2017-08-25 20:53:32 +0300

    small changes

--- src/views/sitemapindex.php
+++ src/views/sitemapindex.php
@@ -1,5 +1,5 @@
 <?= '<'.'?'.'xml version="1.0" encoding="UTF-8"?>'."\n"; ?>
-<?php if ($style != null) echo '<'.'?'.'xml-stylesheet href="'.$style.'" type="text/xsl"?>'."\n"; ?>
+<?php if (null != $style) echo '<'.'?'.'xml-stylesheet href="'.$style.'" type="text/xsl"?>'."\n"; ?>
 <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 <?php foreach($sitemaps as $sitemap) : ?>
 	<sitemap>
